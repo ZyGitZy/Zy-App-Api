f2dc45c11b01ff11a832ba11739a1a6f91019a83
root <1553577703@qq.com>
2023-01-10 10:32:45 +0800

    添加ids引用

--- Zy.App.Api/Program.cs
+++ Zy.App.Api/Program.cs
@@ -1,22 +1,19 @@
 using Zy.App.Common.StoreCore;
-//using Zy.Ids.App.IdsModelExtensions;
+using Zy.Ids.App.IdsModelExtensions;
 using Zy.User.App.IdentityModelExtensions;
 using Zy.Video.App;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
 IConfiguration configuration = builder.Configuration;
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddIdentityModel(configuration);
-//builder.Services.AddIdentityServiceModel();
+builder.Services.AddIdentityServiceModel();
 builder.Services.AddScoped(typeof(IEntityStore<>), typeof(EntityStore<>));
 
 builder.Services.AddCors();
@@ -26,7 +23,6 @@ builder.Services.AddVideoServiceModule(mvc);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
