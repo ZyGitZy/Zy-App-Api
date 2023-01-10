26cb7e761c8f0024e2df37884172a66006018302
root <1553577703@qq.com>
2023-01-10 10:28:36 +0800

    inti

---
+++ Zy.User/Enum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zy.User
+{
+    public enum ActiveStatus
+    {
+        /// <summary>
+        /// 新建
+        /// </summary>
+        New = 1,
+
+        /// <summary>
+        /// 启用状态
+        /// </summary>
+        Active,
+
+        /// <summary>
+        /// 停用状态
+        /// </summary>
+        Inactive,
+    }
+}
